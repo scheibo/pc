cf8839371639b667cf6b92e7b2ef3c813e33f817
Kirk Scheibelhut <kjs@scheibo.com>
2015-04-12 10:36:39 -0700

    Time to start writing

--- test/test.js
+++ test/test.js
@@ -1,10 +1,9 @@
 'use strict';
-var assert = require("assert")
+var assert = require('assert');
 var pc = require('../');
 
 describe('pc node module', function() {
   it('must have at least one test', function() {
-    pc();
     assert(false, 'No tests');
   });
 });
