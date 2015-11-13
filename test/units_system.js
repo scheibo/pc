b52d466d1cb4bf89d1aa7edb7af3707f0e167b15
Kirk Scheibelhut <kjs@google.com>
2015-11-12 17:00:29 -0800

    Some test attempts

--- test/units_system.js
+++ test/units_system.js
@@ -5,10 +5,15 @@ var UnitsSystem = pc.metrics.UnitsSystem;
 
 describe('pc.metrics.UnitsSystem', function() {
   it('validates units', function() {
-    assert.doesNotThrow(function() { UnitsSystem.validate(UnitsSystem.METRIC) });
-    assert.doesNotThrow(function() { UnitsSystem.validate(UnitsSystem.IMPERIAL) });
-    assert.throws(function() { UnitsSystem.validate(undefined) }, /InvalidUnitsSystemException/);
-    assert.throws(function() { UnitsSystem.validate(null) }, /InvalidUnitsSystemException/);
-    assert.throws(function() { UnitsSystem.validate("other") }, /InvalidUnitsSystemException/);
+    assert.doesNotThrow(function() { UnitsSystem.validate(UnitsSystem.METRIC); });
+    assert.doesNotThrow(function() { UnitsSystem.validate(UnitsSystem.IMPERIAL); });
+    assert.throws(function() { UnitsSystem.validate(undefined); }, /InvalidUnitsSystemException/);
+    assert.throws(function() { UnitsSystem.validate(null); }, /InvalidUnitsSystemException/);
+    assert.throws(function() { UnitsSystem.validate("other"); }, /InvalidUnitsSystemException/);
+  });
+  it('infers units', function() {
+    assert.equal(UnitsSystem.infer(UnitsSystem.IMPERIAL), UnitsSystem.IMPERIAL);
+    assert.equal(UnitsSystem.infer(UnitsSystem.METRIC), UnitsSystem.METRIC);
+    assert.equal(UnitsSystem.infer(undefined), UnitsSystem.METRIC);
   });
 });
--- test/weight.js
+++ test/weight.js
@@ -2,8 +2,13 @@
 var assert = require('assert');
 var pc = require('../');
 var Weight = pc.metrics.Weight;
+var UnitsSystem = pc.metrics.UnitsSystem;
 
 describe('pc.metrics.Weight', function() {
-  it('should default to kilogram');
-  it('should understand pounds');
+  it('should default to kilogram', function() {
+    assert.equal(new Weight(10), '10 kg');
+  });
+  it('should understand pounds', function() {
+    assert.equal(new Weight(2, UnitsSystem.IMPERIAL), '2 lb');
+  });
 });
