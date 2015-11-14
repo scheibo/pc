b5078b2a2ebb05643a17365692365ffe1ff64fd1
Kirk Scheibelhut <kjs@google.com>
2015-11-13 16:19:11 -0800

    Fix tests

--- test/weight.js
+++ test/weight.js
@@ -6,9 +6,9 @@ var UnitsSystem = pc.metrics.UnitsSystem;
 
 describe('pc.metrics.Weight', function() {
   it('should default to kilogram', function() {
-    assert.equal(new Weight(10), '10 kg');
+    assert.equal(new Weight(10).toString(UnitsSystem.METRIC), '10 kg');
   });
   it('should understand pounds', function() {
-    assert.equal(new Weight(2, UnitsSystem.IMPERIAL), '2 lb');
+    assert.equal(new Weight(2, UnitsSystem.IMPERIAL).toString(UnitsSystem.IMPERIAL), '2 lb');
   });
 });
