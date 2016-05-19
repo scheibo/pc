904715694fa7e5c4c17075aca32c33f50985505c
Kirk Scheibelhut <kjs@google.com>
2016-05-19 16:53:13 -0700

    Start trying to come up with unit tests for time

---
+++ test/distance.js
@@ -0,0 +1,8 @@
+'use strict';
+var assert = require('assert');
+var pc = require('../');
+var Distance = pc.Distance;
+
+describe('pc.Distance', () => {
+
+});
---
+++ test/speed.js
@@ -0,0 +1,8 @@
+'use strict';
+var assert = require('assert');
+var pc = require('../');
+var Distance = pc.Time;
+
+describe('pc.Speed', () => {
+
+});
